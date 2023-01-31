c8713d8978d5ff6b49b548e5b10069b592ba8047
Ezequiel Orellana <ezeore2@gmail.com>
2023-01-31 00:47:02 -0300

    Curriculum terminado

---
+++ js/Curriculum.js
@@ -0,0 +1,14 @@
+
+document.getElementById('referencia1').onclick=function(){
+    document.getElementById("telefono1").innerHTML="(783) 498 4645";
+    console.log("click capturado");
+}
+
+document.getElementById('referencia2').onclick=function(){
+    document.getElementById("telefono2").innerHTML="(783) 555 5463";
+    console.log("click capturado");
+}
+
+document.getElementById('referencia3').onclick=function(){
+    document.getElementById("telefono3").innerHTML="(783) 455 6464";
+}
